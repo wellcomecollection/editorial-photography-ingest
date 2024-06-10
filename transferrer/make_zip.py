1bf385935e22ac53a6b139e868fa59d8d460e651
Paul Butcher <paul.butcher@gmail.com>
2024-06-10 14:49:05 +0100

    run black

--- transferrer/make_zip.py
+++ transferrer/make_zip.py
@@ -17,7 +17,9 @@ def shoot_number_to_accession_id(accession_number, shoot_number):
     if accession_number and shoot_number:
         return accession_number + "_" + shoot_number
     else:
-        raise ValueError(f"misssing accession or shoot number - accession: '{accession_number}' shoot: '{shoot_number}'")
+        raise ValueError(
+            f"misssing accession or shoot number - accession: '{accession_number}' shoot: '{shoot_number}'"
+        )
 
 
 def generate_metadata_csv(csvfile, accession_id):
@@ -28,17 +30,23 @@ def generate_metadata_csv(csvfile, accession_id):
     >>> generate_metadata_csv(io.StringIO(), shoot_number_to_accession_id("2754", "CP000159")).getvalue()
     'filename,collection_reference,accession_number\\r\\n/objects,WT,2754_CP000159\\r\\n'
     """
-    writer = csv.DictWriter(csvfile, fieldnames=["filename", "collection_reference", "accession_number"])
+    writer = csv.DictWriter(
+        csvfile, fieldnames=["filename", "collection_reference", "accession_number"]
+    )
     writer.writeheader()
     writer.writerow(
-        {"filename": "/objects", "collection_reference": "WT", "accession_number": accession_id}
+        {
+            "filename": "/objects",
+            "collection_reference": "WT",
+            "accession_number": accession_id,
+        }
     )
     return csvfile
 
 
 def create_born_digital_folder(source_files, target_directory, accession_id):
     os.makedirs(target_directory, exist_ok=True)
-    with open(os.path.join(target_directory, 'metadata.csv'), 'w') as csvfile:
+    with open(os.path.join(target_directory, "metadata.csv"), "w") as csvfile:
         generate_metadata_csv(csvfile, accession_id)
     move_files_to_objects_folder(source_files, target_directory)
     return target_directory
@@ -51,17 +59,18 @@ def move_files_to_objects_folder(source_files, target_directory):
     """
     os.makedirs(os.path.join(target_directory, "objects"))
     for file_abspath in source_files:
-        shutil.move(
-            file_abspath,
-            os.path.join(target_directory, "objects")
-        )
+        shutil.move(file_abspath, os.path.join(target_directory, "objects"))
 
 
 def files_in_folder(source_directory):
     """
     Return a list of relevant files in the given directory.
     """
-    return [filename for filename in os.listdir(source_directory) if not discard_file(filename)]
+    return [
+        filename
+        for filename in os.listdir(source_directory)
+        if not discard_file(filename)
+    ]
 
 
 def full_paths(source_directory, filenames):
@@ -75,11 +84,15 @@ def full_paths(source_directory, filenames):
     return (os.path.join(source_directory, filename) for filename in filenames)
 
 
-def create_born_digital_zips(source_directory, target_directory, accession_number, shoot_number, max_batch_size):
+def create_born_digital_zips(
+    source_directory, target_directory, accession_number, shoot_number, max_batch_size
+):
     filenames = files_in_folder(source_directory)
 
     if len(filenames) == 0:
-        raise FileNotFoundError("Attempt to build born digital accession zip from empty folder")
+        raise FileNotFoundError(
+            "Attempt to build born digital accession zip from empty folder"
+        )
 
     if len(filenames) > max_batch_size:
         # In order to produce predictable/repeatable zips, the filename list is sorted before batching.
@@ -87,14 +100,22 @@ def create_born_digital_zips(source_directory, target_directory, accession_numbe
         batches = batched(sorted(filenames), max_batch_size)
         for ix, batch in enumerate(batches, 1):
             accession_id = f"{shoot_number_to_accession_id(accession_number, shoot_number)}_{ix:03d}"
-            yield create_born_digital_zip(full_paths(source_directory, batch), os.path.join(target_directory, accession_id), accession_id)
+            yield create_born_digital_zip(
+                full_paths(source_directory, batch),
+                os.path.join(target_directory, accession_id),
+                accession_id,
+            )
     else:
         accession_id = shoot_number_to_accession_id(accession_number, shoot_number)
-        yield create_born_digital_zip(full_paths(source_directory, filenames), os.path.join(target_directory, accession_id), accession_id)
+        yield create_born_digital_zip(
+            full_paths(source_directory, filenames),
+            os.path.join(target_directory, accession_id),
+            accession_id,
+        )
 
 
 def create_born_digital_zip(source_files, target_directory, accession_id):
-    folder = create_born_digital_folder(source_files,  target_directory, accession_id)
+    folder = create_born_digital_folder(source_files, target_directory, accession_id)
     return shutil.make_archive(accession_id, "zip", folder)
 
 
@@ -102,10 +123,11 @@ def create_born_digital_zip(source_files, target_directory, accession_id):
 # When we start using Python >= 3.12, we can get rid of this
 # but other applications nearby use 3, so I've shimmed it for now.
 
+
 def batched(iterable, n):
     # batched('ABCDEFG', 3) → ABC DEF G
     if n < 1:
-        raise ValueError('n must be at least one')
+        raise ValueError("n must be at least one")
     iterator = iter(iterable)
     while batch := tuple(itertools.islice(iterator, n)):
         yield batch
--- transferrer/test/test_make_zips.py
+++ transferrer/test/test_make_zips.py
@@ -9,7 +9,7 @@ from make_zip import create_born_digital_zips
 
 
 def populate_source_dir_with_images(fs, name, count):
-    populate_source_dir(fs, (f"/in/{name}_{ix:03d}.tif" for ix in range(1, count+1)))
+    populate_source_dir(fs, (f"/in/{name}_{ix:03d}.tif" for ix in range(1, count + 1)))
 
 
 def populate_source_dir(fs, filenames):
@@ -21,12 +21,14 @@ def assert_csv_has_accession_id(csv_path, accession_id):
     """
     Checks that a metadata.csv has the correct format, and that the accession number matches the expected value
     """
-    with open(csv_path, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)  # Ensures that the header row is present and correct
+    with open(csv_path, "r") as csv_file:
+        reader = csv.DictReader(
+            csv_file
+        )  # Ensures that the header row is present and correct
         assert next(reader) == {
             "filename": "/objects",
             "collection_reference": "WT",
-            "accession_number": accession_id
+            "accession_number": accession_id,
         }
         assert not next(reader, False)  # The CSV should have only one row
 
@@ -50,7 +52,7 @@ def test_single_zip(fs):
     next(create_born_digital_zips("/in", "/out", "1234", "G00DCAFE", 10))
     # it creates a zip named using the accession and shoot numbers
     with zipfile.ZipFile("./1234_G00DCAFE.zip") as zip_file:
-        zip_file.extractall('/unzipped')
+        zip_file.extractall("/unzipped")
         # with a metadata csv in the root of the zip
         assert_csv_has_accession_id("/unzipped/metadata.csv", "1234_G00DCAFE")
         # and the photos in an ./objects folder
@@ -66,18 +68,18 @@ def test_multiple_zips(fs):
     list(create_born_digital_zips("/in", "/out", "1234", "G00DCAFE", 10))
     # it creates zips named using the accession and shoot numbers, with a three-digit numeric suffix
     with zipfile.ZipFile("./1234_G00DCAFE_001.zip") as zip_file:
-        zip_file.extractall('/unzipped_001')
+        zip_file.extractall("/unzipped_001")
         assert_csv_has_accession_id("/unzipped_001/metadata.csv", "1234_G00DCAFE_001")
         # The objects chosen for each zip are predictable and consistent.
         # They are sorted alphanumerically before being sliced into groups to place into each zip
-        objects = os.listdir('/unzipped_001/objects')
+        objects = os.listdir("/unzipped_001/objects")
         assert len(objects) == 10
         assert set(filename[:3] for filename in objects) == {"AAA"}
 
     with zipfile.ZipFile("./1234_G00DCAFE_002.zip") as zip_file:
-        zip_file.extractall('/unzipped_002')
+        zip_file.extractall("/unzipped_002")
         assert_csv_has_accession_id("/unzipped_002/metadata.csv", "1234_G00DCAFE_002")
-        objects = os.listdir('/unzipped_002/objects')
+        objects = os.listdir("/unzipped_002/objects")
         assert len(objects) == 10
         assert set(filename[:3] for filename in objects) == {"BBB"}
 
@@ -108,5 +110,3 @@ def test_ignored_files_multiple_zips(fs):
     zips = fnmatch.filter(os.listdir("."), "*zip")
     # 20/5 == 4, ceil 22/5 > 4
     assert len(zips) == 4
-
-
