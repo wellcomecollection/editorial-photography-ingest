1b73bc3512ccf6fc7c56b65d07b90c095ceb0aac
Paul Butcher <paul.butcher@gmail.com>
2024-06-11 16:56:57 +0100

    no need for two Tier statements

--- src/transferrer/restore.py
+++ src/transferrer/restore.py
@@ -3,19 +3,19 @@ import os
 from transferrer.common import should_download_file, shoot_number_to_folder_path, get_source_bucket
 
 
-def restore_s3_folder(bucket, s3_folder: str, days_to_keep=7):
-
+def restore_s3_folder(bucket, s3_folder: str, days_to_keep=1):
+    print(s3_folder)
     for obj in bucket.objects.filter(Prefix=s3_folder):
+        print(obj.key)
         if should_download_file(obj.key):
-            obj.restore_object(
+            print(obj.restore_object(
                 RestoreRequest={
                     'Days': days_to_keep,
                     'GlacierJobParameters': {
                         'Tier': 'Bulk'
-                    },
-                    'Tier': 'Bulk'
+                    }
                 }
-            )
+            ))
 
 
 def restore_shoot_folder(bucket, shoot_number):
@@ -25,6 +25,7 @@ def restore_shoot_folder(bucket, shoot_number):
 def restore_shoot_folders(shoot_numbers):
     bucket = get_source_bucket()
     for shoot_number in shoot_numbers:
+        print(f"restoring {shoot_number}")
         restore_shoot_folder(bucket, shoot_number)
 
 
