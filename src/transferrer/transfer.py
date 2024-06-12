98276fa96fc9faf6519e49993652edbc16090199
Paul Butcher <paul.butcher@gmail.com>
2024-06-12 16:20:11 +0100

    add end(ish)-to-end transfer feature

---
+++ src/transferrer/transfer.py
@@ -0,0 +1,20 @@
+import logging
+import os
+import tempfile
+from transferrer.download import download_shoot
+from transferrer.make_zip import create_born_digital_zips
+from transferrer.upload import upload
+
+
+def transfer_shoot(shoot_number, accession_number):
+    with tempfile.TemporaryDirectory() as tmpfolder:
+        source_folder = os.path.join(tmpfolder, "source")
+        target_folder = os.path.join(tmpfolder, "target")
+        download_shoot(shoot_number, source_folder)
+        for zipfile in create_born_digital_zips(source_folder, target_folder, accession_number, shoot_number, 250):
+            upload(zipfile)
+
+
+if __name__ == "__main__":
+    import sys
+    transfer_shoot(sys.argv[1], "2754")
