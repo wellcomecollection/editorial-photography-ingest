826e8b5a07a11d7ce3de1396ed0c6479c4e10553
Paul Butcher <paul.butcher@gmail.com>
2024-06-12 16:18:54 +0100

    implement uploading (to some folder on stage)

--- src/transferrer/upload.py
+++ src/transferrer/upload.py
@@ -1,6 +1,23 @@
-import boto3
-def get_bucket():
-    session = boto3.Session()
-    S3 = session.resource('s3')
-    return S3.Bucket("wellcomecollection-workflow-stage-upload")
 
+import logging
+import os
+import sys
+
+import boto3
+
+logger = logging.getLogger(__name__)
+
+
+def get_target_bucket(target_bucket):
+    session = boto3.Session()
+    S3 = session.resource('s3', config=)
+    return S3.Bucket(target_bucket)
+
+
+def upload(zipfile_path, target_bucket="wellcomecollection-workflow-stage-upload"):
+    logger.info(f"uploading {zipfile_path} to {target_bucket}")
+    get_target_bucket(target_bucket).upload_file(zipfile_path, f"ep_transfer/{os.path.basename(zipfile_path)}")
+
+
+if __name__ == "__main__":
+    upload(sys.argv[1])
