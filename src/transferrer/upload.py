0606dc8c3bf4a4a00d0bb88adf118fd2fdfc1f8c
Paul Butcher <paul.butcher@gmail.com>
2024-06-14 09:12:50 +0100

    extract profile to env var

--- src/transferrer/upload.py
+++ src/transferrer/upload.py
@@ -8,16 +8,15 @@ import boto3
 logger = logging.getLogger(__name__)
 
 
-def get_target_bucket(target_bucket):
-    session = boto3.Session()
-    S3 = session.resource('s3', config=)
-    return S3.Bucket(target_bucket)
+def get_target_bucket(session, target_bucket):
+    return session.resource('s3').Bucket(target_bucket)
 
 
-def upload(zipfile_path, target_bucket="wellcomecollection-workflow-stage-upload"):
+def upload(session, zipfile_path, target_bucket="wellcomecollection-archivematica-staging-transfer-source"):
     logger.info(f"uploading {zipfile_path} to {target_bucket}")
-    get_target_bucket(target_bucket).upload_file(zipfile_path, f"ep_transfer/{os.path.basename(zipfile_path)}")
+    get_target_bucket(session, target_bucket).upload_file(zipfile_path, f"born-digital-accessions/{os.path.basename(zipfile_path)}")
 
 
 if __name__ == "__main__":
-    upload(sys.argv[1])
+    session = boto3.Session(os.environ["AWS_TARGET_PROFILE"])
+    upload(session, sys.argv[1])
