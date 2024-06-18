6739903c24b3ae7a80132526363fa21be76457eb
Paul Butcher <paul.butcher@gmail.com>
2024-06-18 10:00:47 +0100

    improve test layout

--- src/transferrer/upload.py
+++ src/transferrer/upload.py
@@ -8,15 +8,14 @@ import boto3
 logger = logging.getLogger(__name__)
 
 
-def get_target_bucket(session, target_bucket):
-    return session.resource('s3').Bucket(target_bucket)
+def upload(s3, zipfile_path, target_bucket_name="wellcomecollection-archivematica-staging-transfer-source"):
+    logger.info(f"uploading {zipfile_path} to {target_bucket_name}")
+    get_target_bucket(s3, target_bucket_name).upload_file(zipfile_path, f"born-digital-accessions/{os.path.basename(zipfile_path)}")
 
 
-def upload(session, zipfile_path, target_bucket="wellcomecollection-archivematica-staging-transfer-source"):
-    logger.info(f"uploading {zipfile_path} to {target_bucket}")
-    get_target_bucket(session, target_bucket).upload_file(zipfile_path, f"born-digital-accessions/{os.path.basename(zipfile_path)}")
+def get_target_bucket(s3, target_bucket):
+    return s3.Bucket(target_bucket)
 
 
 if __name__ == "__main__":
-    session = boto3.Session(os.environ["AWS_TARGET_PROFILE"])
-    upload(session, sys.argv[1])
+    upload(boto3.Session(os.environ["AWS_TARGET_PROFILE"]).resource('s3'), sys.argv[1])
